9f1f61279e78c73797162110a710212525f41727
Enemeez <c4rl.hawks@gmail.com>
2020-04-04 14:48:03 -0500

    remove files

--- csgo.py
+++ csgo.py
@@ -2,7 +2,6 @@
 How to use: create a file named 'raw.txt' containing the lines of text you want
 to be bound. This file should be in the same directory as the .py file. Run the script
 and the autoexec.cfg file should be created in the same directory.
-
 '''
 
 with open('raw.txt', 'r') as file, open('autoexec.cfg', 'w') as output:
