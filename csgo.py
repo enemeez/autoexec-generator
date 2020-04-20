c8e8835eca5aeabfd2f8e487144c01715792dd07
Carl <52186431+enemeez@users.noreply.github.com>
2020-04-20 03:14:11 -0500

    Update csgo.py

--- csgo.py
+++ csgo.py
@@ -1,6 +1,6 @@
 '''
 How to use: create a file named 'raw.txt' containing the lines of text you want
-to be bound. This file should be in the same directory as the .py file. Run the script
+to be bound. This file should be in the same directory as the .py file. Run the script,
 and the autoexec.cfg file should be created in the same directory.
 '''
 
